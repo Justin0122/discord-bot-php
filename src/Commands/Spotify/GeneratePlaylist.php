93fdce6ac86463396c14b459e6d9b0d0645667ef
Justin <justinjongstra8@gmail.com>
2023-05-06 21:56:12 +0200

    handle tokens / sessions

--- src/Commands/Spotify/GeneratePlaylist.php
+++ src/Commands/Spotify/GeneratePlaylist.php
@@ -3,6 +3,8 @@
 namespace Bot\Commands\Spotify;
 
 use Bot\Helpers\ErrorHandler;
+use Bot\Helpers\SessionHandler;
+use Bot\Helpers\TokenHandler;
 use bot\Scheduler\PlaylistScheduler;
 
 class GeneratePlaylist
@@ -22,6 +24,19 @@ class GeneratePlaylist
 
     public function handle($args, $discord, $username, $user_id): array
     {
+        $tokenHandler = new TokenHandler($_ENV['API_URL'], $_ENV['SECURE_TOKEN']);
+        $tokens = $tokenHandler->getTokens($user_id);
+
+        if (!$tokens) {
+            return ErrorHandler::handle("Please register using the `spotify` command first.");
+        }
+
+        //set the api using sessionHandler
+        $api = (new SessionHandler())->setSession($user_id);
+
+        $me = $api->me();
+
+        return [];
 
     }
 
--- src/Commands/Spotify/GetLatestSong.php
+++ src/Commands/Spotify/GetLatestSong.php
@@ -3,9 +3,8 @@
 namespace Bot\Commands\Spotify;
 
 use Bot\Helpers\ErrorHandler;
-use GuzzleHttp\Client;
-use SpotifyWebAPI\SpotifyWebAPI;
-use SpotifyWebAPI\Session;
+use Bot\Helpers\SessionHandler;
+use Bot\Helpers\TokenHandler;
 
 class GetLatestSong
 {
@@ -26,56 +25,22 @@ class GetLatestSong
 
     public function handle($args, $discord, $username, $user_id): array
     {
+        $tokenHandler = new TokenHandler($_ENV['API_URL'], $_ENV['SECURE_TOKEN']);
+        $tokens = $tokenHandler->getTokens($user_id);
 
-        $api_url = $_ENV['API_URL'];
-        $secure_token = $_ENV['SECURE_TOKEN'];
-        $discord_id = $user_id;
-
-        $link = "$api_url$discord_id?secure_token=$secure_token&discord_id=$discord_id";
-
-        $client = new Client();
-
-        try{
-        $response = $client->request('GET', $link);
-        } catch (\GuzzleHttp\Exception\ClientException $e) {
+        if (!$tokens) {
             return ErrorHandler::handle("Please register using the `spotify` command first.");
         }
-        $response = json_decode($response->getBody(), true);
 
-        $users = $response['data']['attributes'];
-        $discord_id = $users['discord_id'];
-        $accessToken = $users['spotify_access_token'];
+        //set the api using sessionHandler
+        $api = (new SessionHandler())->setSession($user_id);
 
-        $api = new SpotifyWebAPI();
-        $api->setAccessToken($accessToken);
-
-        try {
-            $me = $api->me();
-        } catch (\SpotifyWebAPI\SpotifyWebAPIException $e) {
-            if ($e->getMessage() == 'The access token expired') {
-                $session = new Session(
-                    $_ENV['SPOTIFY_CLIENT_ID'],
-                    $_ENV['SPOTIFY_CLIENT_SECRET'],
-                    $_ENV['SPOTIFY_REDIRECT_URI']
-                );
-                $session->refreshAccessToken($users['spotify_refresh_token']);
-                $accessToken = $session->getAccessToken();
-                $refreshToken = $session->getRefreshToken();
-                $link = "$api_url?discord_id=$discord_id&secure_token=$secure_token&spotify_access_token=$accessToken&spotify_refresh_token=$refreshToken";
-                $client->request('GET', $link);
-
-                $api->setAccessToken($accessToken);
-                $me = $api->me();
-                echo 'Access token refreshed for ' . $me->display_name . PHP_EOL;
-            } else {
-                return ErrorHandler::handle($e, $discord);
-            }
-        }
-
+        //get the latest 10 songs
         $tracks = $api->getMySavedTracks([
             'limit' => 10
         ]);
 
+        $me = $api->me();
         $embed = [
             'title' => 'Latest songs added by ' . $me->display_name,
             'content' => '',
@@ -93,4 +58,5 @@ class GetLatestSong
         }
         return $embed;
     }
+
 }
